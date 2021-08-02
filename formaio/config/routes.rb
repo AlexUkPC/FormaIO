763374c0b0b184e4dcd36ee441623c7ce1e1e8d3
AlexUkPC <alexrogna@yahoo.com>
2021-08-02 15:47:18 +0100

    Add roles for users Fixes #10

--- formaio/config/routes.rb
+++ formaio/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   end
   root to: "home#index"
   devise_for :users, controllers: { registrations:'registrations' }
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
