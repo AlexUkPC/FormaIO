3e5678182af9fc1b0db9866af86f1f284f39ccad
AlexUkPC <alexrogna@yahoo.com>
2021-08-02 14:07:13 +0100

    Create a menu for authetication/profiles Fixes #14

--- formaio/config/routes.rb
+++ formaio/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  namespace :settings do
+    resource :user, only: [:show, :update]
+  end
   root to: "home#index"
-  devise_for :users
+  devise_for :users, controllers: { registrations:'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
