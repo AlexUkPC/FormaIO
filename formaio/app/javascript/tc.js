9557cc192ebc1bf30d1e75cc4e9cc101e5bd7ce4
AlexUkPC <alexrogna@yahoo.com>
2021-08-10 10:19:57 +0100

    Home page with nav Fixes #39

--- formaio/app/javascript/tc.js
+++ formaio/app/javascript/tc.js
@@ -6,15 +6,20 @@ function show() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const tc = document.getElementById("tc")
-  if (tc.checked==true){
-    show()
+  if (document.getElementById("tc")) {
+    const tc = document.getElementById("tc")
+    if (tc.checked==true){
+      show()
+    }
   }
+  
   document.addEventListener("click", (e) => {
       if (e.target) {
-        const tc = document.getElementById("tc")
-        if (tc.checked==true){
-          show()
+        if (document.getElementById("tc")) {
+          const tc = document.getElementById("tc")
+          if (tc.checked==true){
+            show()
+          }
         }
 
       }
